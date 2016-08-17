7cf00364e793639290d55d9407cc0dc4b37ef8fd
syed nabeel <snabe@allstate.com>
2016-08-17 17:02:12 -0500

    Initial commit

---
+++ src/test/resources/clean-database.sql
@@ -0,0 +1,12 @@
+use tasker_test;
+truncate table tasks;
+INSERT INTO `tasker_test`.`tasks` (`name`, `category`, `due`, `iscomplete`, `createdat`, `updatedat`)
+VALUES ('Get Milk', 'Home', '2016-08-11', b'1', '2011-03-18', '2012-04-19');
+INSERT INTO `tasker_test`.`tasks` (`name`, `category`, `due`, `iscomplete`, `createdat`, `updatedat`)
+VALUES ('Get Bread', 'Home', '2016-08-12', b'0', '2011-03-17', '2012-04-20');
+INSERT INTO `tasker_test`.`tasks` (`name`, `category`, `due`, `iscomplete`, `createdat`, `updatedat`)
+VALUES ('Get Apples', 'Home', '2016-08-13', b'1', '2011-03-16', '2012-04-21');
+INSERT INTO `tasker_test`.`tasks` (`name`, `category`, `due`, `iscomplete`, `createdat`, `updatedat`)
+VALUES ('JavaScript', 'Code', '2016-08-14', b'0', '2011-03-15', '2012-04-22');
+INSERT INTO `tasker_test`.`tasks` (`name`, `category`, `due`, `iscomplete`, `createdat`, `updatedat`)
+VALUES ('Java', 'Code', '2016-08-15', b'1', '2011-03-14', '2012-04-23');
